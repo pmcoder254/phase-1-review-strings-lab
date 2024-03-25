aca65a0037880d61e2fef6d50dbc6cd3bfdba4c9
Paul Muchui <paul.muchungi@gmail.com>
2024-03-25 18:52:01 +0300

    strings are concantenated and use different methods

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
